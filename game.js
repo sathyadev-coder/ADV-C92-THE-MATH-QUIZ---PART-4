87af07fb1e9d2a62b618728a2001f9e37acf7882
sathyadev-coder <88343332+sathyadev-coder@users.noreply.github.com>
2022-09-11 11:05:23 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,115 @@
+player1_name= localStorage.getItem("player1_name");
+player2_name= localStorage.getItem("player2_name");
+
+player1_score=0;
+player2_score=0;
+
+document.getElementById("player1_name").innerHTML=player1_name +":";
+document.getElementById("player2_name").innerHTML=player2_name +":";
+
+document.getElementById("player1_score").innerHTML=player1_score;
+document.getElementById("player2_score").innerHTML=player2_score;
+
+
+document.getElementById("player_question").innerHTML="Question Turn - "+ player1_name;
+document.getElementById("player_answer").innerHTML="Answer Turn- "+ player2_name;
+actual_answer = 0;
+function send() {
+    number1 = document.getElementById("number_1").value;
+    number2 = document.getElementById("number_2").value;
+    actual_answer = parseInt(number1) * parseInt(number2);
+
+
+question_number = "<h4>" + number1 + "X" + number2 + "</h4>";
+
+input_box = "<br> Answer: <input type='text' id='input_check_box'>";
+
+check_button="<br><br><button class='btn btn-info' onclick='check()'>check</button>";
+
+question_number="<h4>" + number1 + "X" + number2 +"</h4>";
+
+input_box="<br>Answer: <input type='text' id='input_check_box'>";
+
+check_button= "<br><br><button class='btn btn-info' onclick='check()'>check</button>";
+
+row= question_number + input_box + check_button;
+
+document.getElementById("output").innerHTML=row;
+
+document.getElementById("number_1").value="";
+
+document.getElementById("number_2").value="";
+
+}
+
+question_turn = "player1";
+answer_turn = "player2";
+
+function check() {
+    get_answer = document.getElementById("input_check_box").value;
+    answer =Number( get_answer);
+    console.log("answer = " + answer)
+    if (answer == actual_answer) {
+        if (answer_turn == "player1") {
+            player1_score = player1_score + 1;
+            updatescore();
+            document.getElementById("player1_score").innerHTML = player1_score;
+
+        } else {
+            player2_score = player2_score + 1;
+            updatescore();
+            document.getElementById("player2_score").innerHTML = player2_score;
+        } 
+    }
+
+    if (question_turn == "player1") {
+        question_turn ="player2"
+        document.getElementById("player_question").innerHTML="Question turn - "+ player2_name;
+        console.log("player2 qu turn");
+    } else {
+        question_turn ="player1"
+        document.getElementById("player_question").innerHTML="Question turn - "+ player1_name;
+        console.log("player1 qu tu");
+    }
+    if(answer_turn == "player1")
+    {
+        answer_turn="player2";
+        document.getElementById("player_answer").innerHTML = "answer turn - "+player2_name;
+        console.log("player2changed ans");
+    }
+    else
+     {
+        answer_turn = "player1";
+        document.getElementById("player_answer").innerHTML = "answer turn - "+player1_name;
+        console.log("player1changed ans");
+    }
+    document.getElementById("output").innerHTML = "";
+
+    document.getElementById("number_1").value="";
+
+    document.getElementById("number_2").value="";
+}
+
+function updatescore()
+{ localStorage.setItem("player1_score",player1_score);
+localStorage.setItem("player2_score",player2_score);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
